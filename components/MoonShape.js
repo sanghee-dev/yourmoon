d9fd14cdd43a9b7c38e7131a5105d375159f94b0
leeedev <leeesangheee@gmail.com>
2021-03-30 17:45:53 +0900

    Make radial gradient moon

--- components/MoonShape.js
+++ components/MoonShape.js
@@ -4,31 +4,60 @@ import { Dimensions } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
-const DIAMETER = 300;
-const MoonContainer = styled.View`
-  width: ${DIAMETER}px;
-  height: ${DIAMETER}px;
-  border-radius: ${DIAMETER / 2}px;
-  background-color: white;
-  overflow: hidden;
+const RADIUS = 1;
+
+const MoonPeaceContainer = styled.View`
+  width: ${WIDTH}px;
+  height: ${WIDTH}px;
+  align-items: center;
 `;
-const MoonShadow = styled.View`
-  width: ${DIAMETER}px;
-  height: ${DIAMETER}px;
-  border-radius: ${DIAMETER / 2}px;
-  background-color: rgba(0, 0, 0, 0.9);
+const MoonPeace = styled.View`
+  width: ${WIDTH / 5}px;
+  height: ${WIDTH}px;
+  position: absolute;
+`;
+const MoonCenter = styled.View`
+  width: 90px;
+  height: 90px;
+  border-radius: 45px;
+  background-color: white;
   position: relative;
+  bottom: ${WIDTH / 2 + 45}px;
 `;
 
 const MoonShape = ({ illumination, leftMoon }) => {
+  const LENGTH = 360;
+  let moonPeaceArray = [];
+  for (let i = 0; i < LENGTH; i++) {
+    moonPeaceArray.push(i);
+  }
+
   return (
-    <MoonContainer>
-      <MoonShadow
-        style={{
-          left: leftMoon ? illumination * 3 : -illumination * 3,
-        }}
-      />
-    </MoonContainer>
+    <>
+      <MoonPeaceContainer>
+        {moonPeaceArray.map((peace) => (
+          <MoonPeace
+            style={[
+              {
+                transform: [{ rotate: `${1 * peace}deg` }],
+              },
+            ]}
+          >
+            <LinearGradient
+              style={{ width: RADIUS, height: WIDTH }}
+              colors={[
+                "rgba(255,255,255,0)",
+                "rgba(255,255,255,1)",
+                "rgba(255,255,255,1)",
+                "rgba(255,255,255,0)",
+              ]}
+              locations={[0, 0.4, 0.6, 1]}
+            />
+          </MoonPeace>
+        ))}
+      </MoonPeaceContainer>
+      <MoonCenter />
+    </>
   );
 };
 
